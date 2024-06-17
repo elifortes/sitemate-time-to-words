dab2e4306dfc93333b6edf133269cb3ce16b4b36
Eliane Fortes <elimrfortes@gmail.com>
2024-06-17 11:38:51 +1000

    adding more comments

--- index.js
+++ index.js
@@ -1,59 +1,35 @@
 // expecting time to be a string in the format like '8:15' or '12:30'
 
-// - '0:00' > 'midnight'
-// - '12:00' > 'midday'
-// - '2:00' > 'two o'clock'
+/*
+- '0:00' > 'midnight'
+- '12:00' > 'midday'
+- '2:00' > 'two o'clock'
+- '2:03' > 'three past two'
+- '2:11' > 'eleven past two'
+- '2:15' > 'quarter past two'
+- '2:30' > 'half past two'
+- '2:33' > 'twenty seven to three'
+- '2:40' > 'twenty to three'
+- '2:45' > 'quarter to three'
+- '2:55' > 'five to three'
+*/
 
-// - '2:03' > 'three past two'
-// - '2:11' > 'eleven past two'
-
-// - '2:15' > 'quarter past two'
-// - '2:30' > 'half past two'
-// - '2:33' > 'twenty seven to three'
-// - '2:40' > 'twenty to three'
-// - '2:45' > 'quarter to three'
-// - '2:55' > 'five to three'
-
-// split - map(Number)
+// use split, separate string
+// map - transform in number
+// edge case - midnight and midday
+// nexthour need increase 1
+// use remaining minutes to handle the quarter to and to
 
 const numberToWords = [
-  "zero",
-  "one",
-  "two",
-  "three",
-  "four",
-  "five",
-  "six",
-  "seven",
-  "eight",
-  "nine",
-  "ten",
-  "eleven",
-  "twelve",
-  "thirteen",
-  "fourteen",
-  "fifteen",
-  "sixteen",
-  "seventeen",
-  "eighteen",
-  "nineteen",
-  "twenty",
-  "twenty one",
-  "twenty two",
-  "twenty three",
-  "twenty four",
-  "twenty five",
-  "twenty six",
-  "twenty seven",
-  "twenty eight",
-  "twenty nine",
+  'zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen',
+  'eighteen', 'nineteen', 'twenty', 'twenty one', 'twenty two', 'twenty three', 'twenty four', 'twenty five', 'twenty six', 'twenty seven', 'twenty eight', 'twenty nine',
 ];
 
 function convertTimeToWords(time) {
-  const [hours, minutes] = time.split(":").map(Number);
+  const [hours, minutes] = time.split(':').map(Number);
 
-  if (hours === 0 && minutes === 0) return "midnight";
-  if (hours === 12 && minutes === 0) return "midday";
+  if (hours === 0 && minutes === 0) return 'midnight';
+  if (hours === 12 && minutes === 0) return 'midday';
   if (minutes === 0) return `${numberToWords[hours]} o' clock`;
 
   const hourText = numberToWords[hours % 12];
@@ -64,7 +40,6 @@ function convertTimeToWords(time) {
   if (minutes === 30) return `half past ${hourText}`;
   if (minutes === 45) return `quarter to ${nextHour}`;
   if (minutes < 30) return `${numberToWords[minutes]} past ${hourText}`;
-
 
   return `${numberToWords[remainingMinutes]} to ${nextHour}`;
 }
